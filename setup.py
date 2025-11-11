5e943a3fd8984d105c004d0ca3ef8c137c639448
Andrea Sevincel <49096189+AndreaSevincel@users.noreply.github.com>
2025-11-11 18:46:13 +0100

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='non-rupert-polyhedra-detector',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Andrea Sevincel',
+    author_email='aemirsevincel@gmail.com',
     description='A project to detect non-Rupert convex polyhedra using algorithms and machine learning.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -18,7 +18,6 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
